032c1164115852de7f1a63aae4d7c5f11acbe52c
♕︎ Tʜᴇ Wʜɪᴛᴇ Dᴇᴠɪʟ ♕︎ <99538849+Tahim15@users.noreply.github.com>
2022-03-14 21:58:42 +0530

    Update youtube_dl_button.py

--- tobrot/helper_funcs/youtube_dl_button.py
+++ tobrot/helper_funcs/youtube_dl_button.py
@@ -34,7 +34,7 @@ async def youtube_dl_call_back(bot, update):
     if current_user_id != current_touched_user_id:
         await bot.answer_callback_query(
             callback_query_id=update.id,
-            text="Dont Touch On This. This Leech isnt started by you..😡😡😡",
+            text="ᴅᴏɴᴛ ᴛᴏᴜᴄʜ ᴏɴ ᴛʜɪs. Tʜɪs ʟᴇᴇᴄʜ ɪs ɴᴏᴛ sᴛᴀʀᴛᴇᴅ ʙʏ ʏᴏᴜ..😡😡😡",
             show_alert=True,
             cache_time=0,
         )
