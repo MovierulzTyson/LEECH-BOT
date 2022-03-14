f354df68c86cd9f323eaa171efada9aca6df0c5f
♕︎ Tʜᴇ Wʜɪᴛᴇ Dᴇᴠɪʟ ♕︎ <99538849+Tahim15@users.noreply.github.com>
2022-03-14 21:37:08 +0530

    Update display_progress.py

--- tobrot/helper_funcs/display_progress.py
+++ tobrot/helper_funcs/display_progress.py
@@ -51,7 +51,7 @@ class Progress:
             [
                 [
                     InlineKeyboardButton(
-                        "Cancel 🚫",
+                        "🚫ᴄᴀɴᴄᴇʟ🚫",
                         callback_data=(
                             f"gUPcancel/{chat_id}/{mes_id}/{from_user}"
                         ).encode("UTF-8"),
@@ -82,13 +82,13 @@ class Progress:
                 ''.join([UN_FINISHED_PROGRESS_STR for i in range(20 - math.floor(percentage / 5))]),
                 round(percentage, 2))
             #cpu = "{psutil.cpu_percent()}%"
-            tmp = progress + "\n**• Total 📀:**`〘{1}〙`\n**• Done ✓ :**` 〘{0}〙`\n**• Speed 🚀 :** `〘{2}〙`\n**• ETA ⏳ :**` 〘{3}〙`".format(
+            tmp = progress + "\n**• ᴛᴏᴛᴀʟ 📀:**`〘{1}〙`\n**• ᴅᴏɴᴇ ✓ :**` 〘{0}〙`\n**• sᴘᴇᴇᴅ 🚀 :** `〘{2}〙`\n**• ᴇᴛᴀ ⏳ :**` 〘{3}〙`".format(
                 humanbytes(current),
                 humanbytes(total),
                 humanbytes(speed),
                 # elapsed_time if elapsed_time != '' else "0 s",
                 estimated_total_time if estimated_total_time != "" else "0 s",
-            #tmp += "\n│"+"\n╰── ⌊ @TGFilmZone ⌉"
+            #tmp += "\n│"+"\n╰── ⌊ @FilmZoneX ⌉"
             )
             try:
                 if not self._mess.photo:
